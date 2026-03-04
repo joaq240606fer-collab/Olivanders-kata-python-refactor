807855ff539ebf86420892cd95b00d1af50539e8
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-03-04 18:15:56 +0100

    feat(Añadir codigo): :sparkles: Añadir clase Backstage
    Añadir clase Backstage

--- Olivanders-kata-python-refactor/src/types.py
+++ Olivanders-kata-python-refactor/src/types.py
@@ -34,3 +34,17 @@ class AgedBrieItem(Item):
         if self.sell_in < 0:
             if self.quality < 50:
                 self.quality += 1
+class BackstagePassesItem(Item):
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+            if self.sell_in < 11:
+                if self.quality < 50:
+                    self.quality += 1
+            if self.sell_in < 6:
+                if self.quality < 50:
+                    self.quality += 1
+        
+        self.sell_in -= 1
+        if self.sell_in < 0:
+            self.quality = 0
