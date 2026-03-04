29d90782e7569f1e639fad3260fd8029289b8f45
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-03-04 18:21:23 +0100

    feat(Añadir codigo): :sparkles: Creacion main.py
    Creacion del main+

--- Olivanders-kata-python-refactor/main.py
+++ Olivanders-kata-python-refactor/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from olivanders-kata-python-refactor!")
+# Crear ítems usando la fábrica
+items = [
+    ItemFactory.create_item("Aged Brie", 2, 0),
+    ItemFactory.create_item("Backstage passes to a TAFKAL80ETC concert", 20, 40),
+    ItemFactory.create_item("Sulfuras, Hand of Ragnaros", 0, 80),
+    ItemFactory.create_item("Conjured Mana Cake", 3, 6), # Usará NormalItem por defecto
+]
 
+# Inicializar el servicio
+gilded_rose = GildedRose(items)
 
-if __name__ == "__main__":
-    main()
+# Actualizar calidad
+gilded_rose.update_quality()
+
+# Ver resultados
+for item in items:
+    print(item)
