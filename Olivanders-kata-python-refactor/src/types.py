54d0e2695dbfa608fde31193c60695e7c7fe8be6
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-03-04 18:17:53 +0100

    feat(Añadir codigo): :sparkles: Añadimos clase olivanders
    clase olivandeers

--- Olivanders-kata-python-refactor/src/types.py
+++ Olivanders-kata-python-refactor/src/types.py
@@ -52,3 +52,11 @@ class BackstagePassesItem(Item):
 class SulfurasItem(Item):
     def update_quality(self):
         pass
+
+class olivanders:
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
